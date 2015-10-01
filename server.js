2f281181872c3dd66565a85ec47cf684e51025b9
Achim Wolf Sperling <sperling@devid.net>
2015-10-01 08:48:54 +0200

    Organisation

--- server.js
+++ server.js
@@ -29,85 +29,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-
-app.get('/', function (req, res) {
-  res.json({ message: 'Hello World!'});
-});
-
-/////////////////
-// Create Tour //
-/////////////////
-app.post('/tours', function(req, res) {
-
-  // Validate submitted data
-  if (new Date(req.body.time) < new Date()) {
-    return res.status(400).json({
-      error: 'Invalid paramters provided.'
-    });
-  }
-
-  res.status(201).json(
-    {
-      id: testingID,
-      status: '1'
-    }
-  );
-});
-
-/////////////////////
-// Get Tour status //
-/////////////////////
-app.get('/tours/:tourId', function(req, res) {
-
-  // Create an ETA set 30 minutes in the future
-  var d = new Date();
-  d.setMinutes(d.getMinutes() + 30);
-
-  res.json(
-    {
-      id: testingID,
-      status: '1',
-      eta: d.toISOString()
-    }
-  );
-});
-
-/////////////////
-// Cancel Tour //
-/////////////////
-app.delete('/tours/:tourId', function(req, res) {
-  res.status(204).end();
-});
-
-/////////////////
-// Update Tour //
-/////////////////
-app.put('/tours/:tourId', function(req, res) {
-
-  // app.params contains all params
-  console.log('Updating Tour %s', req.params.tourId);
-
-  res.json(
-    {
-      id: testingID,
-      status: '1'
-    }
-  );
-});
-
-
-/////////////////////
-// Param callbacks //
-/////////////////////
-
-app.param('tourId', function(req, res, next, id) {
-  if (id !== testingID) {
-    return res.status(404).json({
-      error: 'Not found'
-    });
-  }
-  next();
-});
+// Require routes
+require('./app/routes')(app);
 
 ///////////////////
 // Error handler //
